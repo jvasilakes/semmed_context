0e3fbeab3791f78da4fb6a331023704ad3e09775
Jake Vasilakes <jvasilakes@gmail.com>
2023-05-25 17:08:42 +0100

    Refactor to only use WebDataset

--- modeling/run.py
+++ modeling/run.py
@@ -1,4 +1,5 @@
 import os
+import json
 import argparse
 import warnings
 from glob import glob
@@ -11,7 +12,7 @@ from pytorch_lightning.callbacks import ModelCheckpoint
 from pytorch_lightning.loggers import TensorBoardLogger
 
 from config import config
-from data import SemRepFactDataModule, SemRepFactWebDataModule
+from data import SemRepFactDataModule
 from models import MODEL_REGISTRY
 
 
@@ -28,6 +29,12 @@ def parse_args():
         "config_file", type=str, help="Path to yaml config file.")
     val_parser.add_argument("--split", type=str, default="val",
                             choices=["train", "val", "test"])
+
+    predict_parser = subparsers.add_parser("predict", help="Run prediction")
+    predict_parser.add_argument(
+        "config_file", type=str, help="Path to yaml config file.")
+    predict_parser.add_argument("--split", type=str, default="val",
+                                choices=["train", "val", "test"])
 
     return parser.parse_args()
 
@@ -48,6 +55,9 @@ def main(args):
     elif args.command == "validate":
         run_fn = run_validate
         run_kwargs["datasplit"] = args.split
+    elif args.command == "predict":
+        run_fn = run_predict
+        run_kwargs["datasplit"] = args.split
     else:
         raise argparse.ArgumentError(f"Unknown command '{args.command}'.")
     run_fn(config, **run_kwargs)
@@ -66,11 +76,7 @@ def run_train(config):
     os.makedirs(version_dir, exist_ok=False)
     config.yaml(outpath=os.path.join(version_dir, "config.yaml"))
 
-    targlob = os.path.join(config.Data.datadir.value, "*.tar")
-    if len(glob(targlob)) > 0:
-        datamodule = SemRepFactWebDataModule(config)
-    else:
-        datamodule = SemRepFactDataModule(config)
+    datamodule = SemRepFactDataModule(config)
     datamodule.setup()
 
     model_class = MODEL_REGISTRY[config.Model.model_name.value]
@@ -102,15 +108,14 @@ def run_train(config):
 
 
 def run_validate(config, datasplit):
-    targlob = os.path.join(config.Data.datadir.value, "*.tar")
-    if len(glob(targlob)) > 0:
-        datamodule = SemRepFactWebDataModule(config)
-    else:
-        datamodule = SemRepFactDataModule(config)
+    datamodule = SemRepFactDataModule(config)
     datamodule.setup()
 
     model_class = MODEL_REGISTRY[config.Model.model_name.value]
-    model = model_class.from_config(config, datamodule.label_spec)
+    ckpt_path, hparams_path = find_model_checkpoint(config)
+    model = model_class.load_from_checkpoint(
+        checkpoint_path=ckpt_path,
+        hparams_file=hparams_path)
     model.eval()
 
     if torch.cuda.is_available():
@@ -139,6 +144,106 @@ def run_validate(config, datasplit):
     table = format_results_as_markdown_table(results)
     print("\nResults")
     print(table)
+
+
+def run_predict(config, datasplit):
+    datamodule = SemRepFactDataModule(config)
+    datamodule.setup()
+
+    model_class = MODEL_REGISTRY[config.Model.model_name.value]
+    ckpt_path, hparams_path = find_model_checkpoint(config)
+    model = model_class.load_from_checkpoint(
+        checkpoint_path=ckpt_path,
+        hparams_file=hparams_path)
+    model.eval()
+
+    if torch.cuda.is_available():
+        available_gpus = min(1, torch.cuda.device_count())
+    else:
+        available_gpus = 0
+        warnings.warn("No CUDA devices found. Using CPU.")
+    print(f"GPUs: {available_gpus}")
+
+    trainer = pl.Trainer(
+        logger=False,
+        deterministic=True,
+        gpus=available_gpus)
+
+    if datasplit == "train":
+        predict_dataloader_fn = datamodule.train_dataloader
+    elif datasplit == "val":
+        predict_dataloader_fn = datamodule.val_dataloader
+    elif datasplit == "test":
+        predict_dataloader_fn = datamodule.test_dataloader
+    else:
+        raise ValueError(f"Unknown data split '{datasplit}'")
+    predict_dataloader = predict_dataloader_fn()
+    preds = trainer.predict(
+        model, dataloaders=predict_dataloader)
+
+    outdir = os.path.join(
+        config.Experiment.logdir.value,
+        config.Experiment.name.value,
+        f"version_{config.Experiment.version.value}",
+        "predictions")
+    os.makedirs(outdir, exist_ok=True)
+    outfile = os.path.join(outdir, f"{datasplit}.jsonl")
+
+    ignore_keys = ["token_type_ids", "attention_mask", "position_ids"]
+    preds = unbatch(preds, ignore_keys=ignore_keys)
+    for pred in preds:
+        with open(outfile, 'w') as outF:
+            for pred in preds:
+                json.dump(pred, outF)
+                outF.write('\n')
+
+
+def find_model_checkpoint(config):
+    logdir = os.path.join(
+        config.Experiment.logdir.value,
+        config.Experiment.name.value,
+        f"version_{config.Experiment.version.value}")
+    hparams_path = os.path.join(logdir, "hparams.yaml")
+    if not os.path.isfile(hparams_path):
+        raise OSError(f"No hparams.yaml found in {logdir}.")
+
+    ckpt_glob = os.path.join(logdir, "checkpoints", "*.ckpt")
+    ckpt_files = glob(ckpt_glob)
+    if len(ckpt_files) == 0:
+        raise OSError(f"No checkpoints found at {ckpt_glob}.")
+    return ckpt_files[0], hparams_path
+
+
+def unbatch(batches, ignore_keys=[]):
+    unbatched = []
+    for batch in batches:
+        first_key = list(batch.keys())[0]
+        batch_size = len(batch[first_key])
+        for i in range(batch_size):
+            datum = {}
+            for (key, value) in batch.items():
+                if key in ignore_keys:
+                    continue
+                if isinstance(value, dict):
+                    converted = unbatch([value], ignore_keys=ignore_keys)[i]
+                else:
+                    converted = maybe_convert(batch[key][i])
+                datum[key] = converted
+            unbatched.append(datum)
+    return unbatched
+
+
+def maybe_convert(value):
+    """
+    Check if a value is a tensor, and convert
+    it back to a standard Python object if it is.
+    """
+    if torch.is_tensor(value):
+        if value.dim() == 0:
+            return value.item()
+        else:
+            return value.tolist()
+    return value
 
 
 def format_results_as_markdown_table(results_dict):
