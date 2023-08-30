abc23fd5d5226088caf8e3a5c27aef4f1dda8b6b
Jake Vasilakes <jvasilakes@gmail.com>
2023-08-30 17:31:03 +0100

    Add option to summarize by predicate

--- scripts/summarize_semrep_data.py
+++ scripts/summarize_semrep_data.py
@@ -11,6 +11,8 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("anndir", type=str,
                         help="Directory containing .ann files.")
+    parser.add_argument("--per-predicate", action="store_true", default=False,
+                        help="Compute counts per predicate.")
     return parser.parse_args()
 
 
@@ -19,7 +21,10 @@ def main(args):
     annglob = glob(os.path.join(args.anndir, "*.ann"))
     all_counts = None
     for annfile in tqdm(annglob):
-        counts = summarize_brat_file(annfile)
+        if args.per_predicate is True:
+            counts = summarize_brat_file_per_predicate(annfile)
+        else:
+            counts = summarize_brat_file(annfile)
         if all_counts is None:
             all_counts = counts
         else:
@@ -31,17 +36,34 @@ def main(args):
 def summarize_brat_file(annfile):
     counts = {"num_events": 0,
               "num_attributes": 0,
+              "Predicate": defaultdict(int),
               "Factuality": defaultdict(int),
               "Polarity": defaultdict(int),
               "Certainty": defaultdict(int)}
 
     anns = BratAnnotations.from_file(annfile)
     for event in anns.events:
+        counts["Predicate"][event.type] += 1
         counts["num_events"] += 1
         for (attr_name, attr) in event.attributes.items():
             counts["num_attributes"] += 1
             if attr_name in counts.keys():
                 counts[attr_name][attr.value] += 1
+    return counts
+
+
+def summarize_brat_file_per_predicate(annfile):
+    counts = {"num_events": 0,
+              "num_attributes": 0,
+              "Predicate": defaultdict(lambda: defaultdict(
+                  lambda: defaultdict(int)))}
+
+    anns = BratAnnotations.from_file(annfile)
+    for event in anns.events:
+        counts["num_events"] += 1
+        for (attr_name, attr) in event.attributes.items():
+            counts["num_attributes"] += 1
+            counts["Predicate"][event.type][attr_name][attr.value] += 1
     return counts
 
 
@@ -64,7 +86,10 @@ def print_counts(counts, indent=0, num_events=None):
             print(f"{' ' * indent}{key}:")
             print_counts(counts[key], indent=indent+2, num_events=num_events)
         else:
-            percent = 100 * (count / num_events)
+            try:
+                percent = 100 * (count / num_events)
+            except ZeroDivisionError:
+                percent = 0
             print(f"{' ' * indent}{key}: {count} ({percent:.2f}%)")
 
 
