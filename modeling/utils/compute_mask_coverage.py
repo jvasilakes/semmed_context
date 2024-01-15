14ffaa0832a2bcb6251cf9f40279e45a7e935850
Jake Vasilakes <jvasilakes@gmail.com>
2024-01-15 10:44:34 +0000

    upload mask coverage script

--- modeling/utils/compute_mask_coverage.py
+++ modeling/utils/compute_mask_coverage.py
@@ -2,6 +2,7 @@ import json
 import argparse
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def parse_args():
@@ -15,7 +16,13 @@ def main(args):
     preds = [json.loads(line) for line in open(args.predictions_file)]
     masks = [np.array(pred["json"]["token_mask"]) for pred in preds]
     coverage = [(mask > 0.0).mean() for mask in masks]
-    print(np.mean(coverage), np.std(coverage))
+    print("Coverage: ", np.mean(coverage), np.std(coverage))
+    masks_flat = np.concatenate(masks)
+    masks_flat = masks_flat[masks_flat > 0.0]
+    print("Weight distribution: ", np.mean(masks_flat), np.std(masks_flat))
+    plt.hist(masks_flat, bins=100)
+    plt.title("Attention Weights")
+    plt.show()
 
 
 if __name__ == "__main__":
